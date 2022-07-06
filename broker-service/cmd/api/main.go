ceea9e184897a24cf77e5e0ffdbaad6b648f00b2
Tcharles Pereira da Silva <tcharlezin@gmail.com>
2022-07-06 08:10:41 -0300

    first commit

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broken service on port %s\n", webPort)
+
+	// Define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// Start server
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
